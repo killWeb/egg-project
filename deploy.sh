b849c4937c0be29bdb38ddff5ce1b6040731e0dd
魏毅博 <yibo.wei@17zuoye.com>
2021-07-23 00:58:30 +0800

    --unsafe-perm

--- deploy.sh
+++ deploy.sh
@@ -5,11 +5,11 @@ diffInfo=`git diff --name-only HEAD^`
 if [ ! -d "$workDir/node_modules" ];
 then
     echo "node_modules目录缺失，开始安装依赖"
-    npm i
+    npm i --unsafe-perm
 elif [[ $diffInfo =~ "package.json" ]];
 then
     echo "package.json文件有变动，开始安装依赖"
-    npm i
+    npm i --unsafe-perm
 else
     echo "package.json文件无变动，无需安装依赖"
 fi
